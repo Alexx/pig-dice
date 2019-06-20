cd52a1bf03908b3086c17a5e7e46ebc43002b4fa
Alex Siegberg and Jacob Irwin <alex@>
2019-06-20 16:49:15 -0700

    clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -43,10 +43,6 @@ function resetGame(gamerGroup) {
   $(".inputGamer").show();
 }
 
-function rollDice() {
-  return Math.floor(Math.random() * (7 - 1) + 1);
-}
-
 function playTurn(gamerGroup, result) {
   $("#rolledOneOutput").text("");
   var gamer = gamerGroup.gamers[gamerGroup.currentGamer];
@@ -127,29 +123,29 @@ function isPlayerWinner(gamerGroup) {
 }
 function rollNewDice(result){
   $(this).data('n',$(this).data('n')?0:5);
-    var n = $(this).data('n');
-    $('.cube').attr('style','');
-    angle = {x:360*n,y:360*n}
-    switch (result){
-      case 1:
-        break;
-      case 2:
-        angle.y = 360*n + 90;
-        break;
-      case 3:
-        angle.x = 360*n + 90;
-        break;
-      case 4:
-        angle.x = 360*n - 90;
-        break;
-      case 5:
-        angle.y = 360*n - 90;
-        break;
-      case 6:
-        angle.x = 360*n + 180;
-        break;
-    }
-    $('.cube').css({'-webkit-transform':'translateZ(-100px) rotateX(' + angle.x + 'deg) rotateY(' + angle.y + 'deg)','-webkit-transition':'3s'})
+  var n = $(this).data('n');
+  $('.cube').attr('style','');
+  angle = {x:360*n,y:360*n}
+  switch (result){
+    case 1:
+    break;
+    case 2:
+    angle.y = 360*n + 90;
+    break;
+    case 3:
+    angle.x = 360*n + 90;
+    break;
+    case 4:
+    angle.x = 360*n - 90;
+    break;
+    case 5:
+    angle.y = 360*n - 90;
+    break;
+    case 6:
+    angle.x = 360*n + 180;
+    break;
+  }
+  $('.cube').css({'-webkit-transform':'translateZ(-100px) rotateX(' + angle.x + 'deg) rotateY(' + angle.y + 'deg)','-webkit-transition':'3s'})
 
 }
 
@@ -175,14 +171,14 @@ $(document).ready(function(){
   });
 
   $('.option').on('click','li',function(){
-  $('.cube').attr('class','cube '+$(this).attr('data-opt'))
-}).on('click','.select',function(){
-  hold(gamerGang)
-}).on('click','.roll',function(){
-  var result = Math.round(Math.random()*5 + 1);
-  var angle = {};
-  playTurn(gamerGang, result)
-  console.log(result);
-  rollNewDice(result);
-})
+    $('.cube').attr('class','cube '+$(this).attr('data-opt'))
+  }).on('click','.select',function(){
+    hold(gamerGang)
+  }).on('click','.roll',function(){
+    var result = Math.round(Math.random()*5 + 1);
+    var angle = {};
+    playTurn(gamerGang, result)
+    console.log(result);
+    rollNewDice(result);
+  })
 });
