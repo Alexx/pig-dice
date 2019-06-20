030eefb12c9e30aeac482e6c5285a9a9e20c4d69
Alex Siegberg and Jacob Irwin <jacob@>
2019-06-20 11:42:52 -0700

    add user interface buttons, and fix bugs in backend. fully operational game.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,15 @@
 //-----------Business Logic-------------
-const WinningScore = 10;
+const WinningScore = 100;
+
+//-----------Contructors-------------
 
 function Game() {
   this.gamers = [];
   this.currentGamer = 0;
-  this.currentID = 0;
 }
 
 Game.prototype.addPlayer = function(gamer) {
-  gamer.id = this.assignID();
   this.gamers.push(gamer);
-}
-
-Game.prototype.assignID = function() {
-  this.currentID += 1;
-  return this.currentID;
 }
 
 function Gamer(name) {
@@ -25,41 +20,48 @@ function Gamer(name) {
 
 };
 
-//---------------GAME-----------------
+//---------------GAME FUNCTIONS-----------------
 
-function playTurn(gamerGroup) {
-  var gamer = gamerGroup.gamers[gamerGroup.currentGamer];
-  $("#nameOutput").text(gamer.name + " is currently playing")
-  var currentRoll = rollDice();
-  console.log("Current Dice Roll: " + currentRoll);
-  if(currentRoll === 1) {
-    gamer.currentScore = 0;
-    gamer.diceRolled = [];
-    changeTurn(gamerGroup);
+function checkPlayers(gamerGroup) {
+  if(gamerGroup.gamers.length >= 1) {
+    $(".errorOutput").text("")
+    startGame(gamerGroup);
   } else {
-    gamer.currentScore += currentRoll;
-    gamer.diceRolled.push(currentRoll);
-    $("#diceOutput").text(gamer.diceRolled);
-    if(isPlayerWinner(gamerGroup)) {
-      $("#nameOutput").text(gamer.name + " is the Winnner!!!");
-    }
+    $(".errorOutput").text("Add a player!")
   }
 }
 
-function hold(gamerGroup) {
-  changeTurn(gamerGroup);
-  var gamer = gamerGroup.gamers[gamerGroup.currentGamer];
-  $("#nameOutput").text(gamer.name + " is currently playing")
-  $("#diceOutput").text(gamer.diceRolled);
-  gamer.totalScore += gamer.currentScore;
-  gamer.currentScore = 0;
+function startGame(gamerGroup) {
+  $(".hiddenGame").show();
+  $(".inputGamer").hide();
+  displayInformation(gamerGroup)
+}
 
-  console.log("Current Gamers Total Score: " + gamer.totalScore, "Current Gamers Current Score: " + gamer.currentScore + "current gamer " + gamer.name);
-
+function resetGame(gamerGroup) {
+  reset(gamerGroup)
+  $(".hiddenGame").hide();
+  $(".inputGamer").show();
 }
 
 function rollDice() {
   return Math.floor(Math.random() * (7 - 1) + 1);
+}
+
+function playTurn(gamerGroup) {
+  $("#rolledOneOutput").text("");
+  var gamer = gamerGroup.gamers[gamerGroup.currentGamer];
+  var currentRoll = rollDice();
+  if(currentRoll === 1) {
+    gamer.currentScore = 0;
+    gamer.diceRolled = [];
+    $("#rolledOneOutput").text("YOU ROLLED A ONE LOSER");
+    displayInformation(gamerGroup);
+    changeTurn(gamerGroup);
+  } else {
+    gamer.currentScore += currentRoll;
+    gamer.diceRolled.push(currentRoll);
+    displayInformation(gamerGroup);
+  }
 }
 
 function changeTurn(gamerGroup) {
@@ -71,46 +73,49 @@ function changeTurn(gamerGroup) {
   }
 }
 
-//-----------------------------
-
-//shows winner, score and resets all scores to 0
-function endGame(gamerGroup) {
+function hold(gamerGroup) {
   var gamer = gamerGroup.gamers[gamerGroup.currentGamer];
+  gamer.totalScore += gamer.currentScore;
+  gamer.currentScore = 0;
+  changeTurn(gamerGroup);
+  displayInformation(gamerGroup);
+}
 
-
-  reset(gamerGroup);
-};
-
-//restarts game
 function reset(gamerGroup) {
   gamerGroup.gamers.forEach(function(player) {
     player.diceRolled = [];
     player.totalScore = 0;
     player.currentScore = 0;
   });
-
 };
 
-function isPlayerWinner(gamerGroup) {
-    if(gamerGroup.gamers[gamerGroup.currentGamer].totalScore + gamerGroup.gamers[gamerGroup.currentGamer].currentScore > WinningScore) {
-      return true;
-      console.log("game end");
-      endGame(gamerGroup);
-    } else {
-      return false;
-    }
-}
-function startGame() {
-  $(".hiddenGame").show();
-  $(".inputGamer").hide();
+function displayInformation(gamerGroup) {
+  var gamer = gamerGroup.gamers[gamerGroup.currentGamer];
+  $("#diceOutput").text(gamer.diceRolled);
+  if(isPlayerWinner(gamerGroup)) {
+    reset(gamerGroup);
+    $("#nameOutput").text(gamer.name + " is the Winnner!!!");
+    $("#totalScoreOutput").text("Total Score: " + gamer.totalScore);
+    $("#currentScoreOutput").text("Current Score: " + gamer.currentScore);
+  } else {
+    $("#nameOutput").text(gamer.name + " is currently playing");
+    $("#totalScoreOutput").text("Total Score: " + gamer.totalScore);
+    $("#currentScoreOutput").text("Current Score: " + gamer.currentScore);
+  }
 }
 
-function resetGame() {
-  $(".hiddenGame").hide();
-  $(".inputGamer").show();
+function isPlayerWinner(gamerGroup) {
+  if(gamerGroup.gamers[gamerGroup.currentGamer].totalScore + gamerGroup.gamers[gamerGroup.currentGamer].currentScore >= WinningScore) {
+    reset(gamerGroup);
+    return true;
+  } else {
+    return false;
+  }
 }
+
 
 //------------User Interface--------------
+
 $(document).ready(function(){
   var gamerGang = new Game;
   $("#gamerForm").submit(function(event) {
@@ -118,25 +123,23 @@ $(document).ready(function(){
     var playerName = $("#newPlayer").val();
     newPlayer = new Gamer(playerName);
     gamerGang.addPlayer(newPlayer);
-    console.log(gamerGang);
+    $("#newPlayer").val('');
   });
 
   $("#resetButton").click(function() {
+    resetGame(gamerGang);
+  });
 
+  $("#startButton").click(function() {
+    checkPlayers(gamerGang);
   });
-  $("#startGameButton").click(function() {
-    startGame();
-  });
+
   $("#roll").click(function() {
-      playTurn(gamerGang);
+    playTurn(gamerGang);
   });
 
   $("#hold").click(function() {
-      hold(gamerGang);
+    hold(gamerGang);
   });
 
-  // while (gamerGang.gamers[0].totalScore < 10 && gamerGang.gamers[1].totalScore < 10) {
-    //
-    // }
-
-  });
+});
